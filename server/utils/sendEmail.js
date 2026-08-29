58a4bae68b0b57245477be95f6191e8d1c2c5f90
Dua Batool <duab1407@gmail.com>
2026-08-29 18:34:53 +0500

    fix: use explicit SMTP host configuration

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,16 +2,16 @@ import nodemailer from "nodemailer";
 
 export async function sendEmail(to, subject, html) {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    // Add these to fix timeout
-    port: 465,
-    secure: true,
     connectionTimeout: 30000,
     greetingTimeout: 30000,
+    socketTimeout: 30000,
   });
 
   await transporter.sendMail({
