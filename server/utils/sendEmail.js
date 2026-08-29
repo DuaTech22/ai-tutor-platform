4390503b4c96bb930bb071508305eac0177593a5
Dua Batool <duab1407@gmail.com>
2026-08-29 18:32:36 +0500

    fix: add port 465 and timeouts to email config

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -7,6 +7,11 @@ export async function sendEmail(to, subject, html) {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    // Add these to fix timeout
+    port: 465,
+    secure: true,
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
   });
 
   await transporter.sendMail({
