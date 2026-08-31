6801e94739bc2d55d2f26a056ca66b2d959be5b7
Dua Batool <duab1407@gmail.com>
2026-08-31 23:03:19 +0500

    fix: use port 587 with STARTTLS for email sending on Railway

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,13 +3,14 @@ import nodemailer from "nodemailer";
 export async function sendEmail(to, subject, html) {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    family: 4, // Force IPv4 to avoid ENETUNREACH errors in some hosting environments
+    family: 4,
   });
 
   await transporter.sendMail({
