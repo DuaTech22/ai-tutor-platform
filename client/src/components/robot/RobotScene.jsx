7c652c7796cc40e06b9c9a5ff743b14b3c237abe
Dua Batool <duab1407@gmail.com>
2026-09-04 14:47:50 +0500

    fix: correct Suspense import in RobotScene.jsx (was incorrectly imported from drei)

--- client/src/components/robot/RobotScene.jsx
+++ client/src/components/robot/RobotScene.jsx
@@ -1,22 +1,16 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Suspense } from "@react-three/drei";
+import { Suspense } from "react";
 import RobotModel from "./RobotModel.jsx";
 
-function RobotScene({ emotion = "idle" }) {
+function RobotScene({ emotion }) {
   return (
-    <div className="w-[200px] sm:w-[240px] md:w-[280px] h-[200px] sm:h-[240px] md:h-[280px]">
-      <Canvas camera={{ position: [2, 2, 5], fov: 45 }}>
-        <ambientLight intensity={0.5} />
+    <div className="w-full max-w-sm h-[320px] mx-auto">
+      <Canvas camera={{ fov: 40 }}>
+        <ambientLight intensity={0.8} />
         <directionalLight position={[5, 5, 5]} intensity={1} />
-        <pointLight position={[-5, 0, 5]} intensity={0.5} />
         <Suspense fallback={null}>
           <RobotModel emotion={emotion} />
         </Suspense>
-        <OrbitControls
-          enableZoom={false}
-          enablePan={false}
-          enableRotate={false}
-        />
       </Canvas>
     </div>
   );
